95462e1f043143eb4f98d52c9a912112c0d9879c
Nikita Kozlov <nkozlov@scaleway.com>
2018-11-16 13:45:47 +0100

    add a sentry-msg script to send message to sentry from the shell

---
+++ sentry_msg/__init__.py
@@ -0,0 +1,40 @@
+import argparse
+import os
+
+import raven
+
+__version__ = '2.2.0'
+
+
+def execute():
+    """ sentry-wrapper entrypoint.
+    """
+    parser = argparse.ArgumentParser(
+        usage='%(prog)s [options] [message to send]',
+        epilog=('Example: '
+                'sentry-msg --dsn https://... "send me"')
+    )
+
+    parser.add_argument(
+        'msg',
+        help='Message to send'
+    )
+    parser.add_argument(
+        '--dsn', metavar='SENTRY_DSN', default=os.getenv('SENTRY_DSN'),
+        help='Sentry DSN'
+    )
+
+    args = parser.parse_args()
+
+    if args.dsn is None:
+        parser.error('You must provide sentry DSN from the commandline '
+                     'argument --dsn or from the environment variable '
+                     'SENTRY_DSN')
+
+    if args.msg is None:
+        parser.error('Nothing to send')
+
+    sentry_client = raven.Client(args.dsn)
+    sentry_client.captureMessage(args.msg)
+
+    return 0
